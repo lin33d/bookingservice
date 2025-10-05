9846b1cbd67d3e39f1f16bc6e8848ad77ec10b7c
lin33d <fishchev.den@mail.ru>
2025-10-05 21:06:50 +0400

    init add_booking

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,7 +1,66 @@
-from sqlalchemy import select
+from sqlalchemy import insert, select, and_, or_, func
 from app.dao.base import BaseDAO
 from app.database import async_session_maker
 from app.bookings.models import Bookings
+from app.rooms.models import Rooms
+from app.database import engine, async_session_maker
 
 class BookingDAO(BaseDAO):
     model = Bookings
+
+    @classmethod
+    async def add(
+        cls,
+        user_id: int,
+        room_id: int,
+        date_from: int,
+        date_to: int
+):
+        async with async_session_maker() as session:
+            booked_rooms = select(Bookings).where(
+                and_(
+                    Bookings.room_id == 1,
+                    or_(
+                    and_(
+                            Bookings.date_from >= date_from,
+                            Bookings.date_from <= date_to
+                    ),
+                    and_(
+                            Bookings.date_from <= date_from,
+                            Bookings.date_to >= date_from
+                    ),
+                        
+                    )
+                )
+            ).cte("booked_rooms")
+
+
+            get_rooms_left = select(
+                (Rooms.quantity - func.count(booked_rooms.c.room_id)).label("rooms_left")
+                ).select_from(Rooms).join(
+                    booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True
+                ).where(Rooms.id == 1).group_by(
+                    Rooms.quantity, booked_rooms.c.room_id
+                )
+                
+            print(get_rooms_left.compile(engine, compile_kwargs={"literal_binds": True}))
+            rooms_left = await session.execute(get_rooms_left)
+            rooms_left: int = rooms_left.scalar()
+            if rooms_left > 0:
+                get_price = select(Rooms.price).filter_by(id=room_id)
+                price = await session.execute(get_price)
+                price: int = price.scalar()
+                add_booking = insert(Bookings).values(
+                    room_id=room_id,
+                    user_id=user_id,
+                    date_from=date_from,
+                    date_to=date_to,
+                    price=price,
+                ).returning(Bookings)
+
+                new_booking = await session.execute(add_booking)
+                await session.commit()
+                return new_booking.scalar()
+                
+            else:
+                return None
--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,9 +1,11 @@
+from datetime import date
 from fastapi import APIRouter, Depends, Request
 from sqlalchemy import select
 from app.bookings.dao import BookingDAO
 from app.database import async_session_maker
 from app.bookings.models import Bookings
 from app.bookings.schemas import SBooking
+from app.exceptions import RoomCannotBeBooked
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 
@@ -15,6 +17,14 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings(user: Users = Depends(get_current_user)): #-> list[SBooking]:
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[SBooking]:
     return await BookingDAO.find_all(user_id=user.id)
-    
+
+@router.post("")
+async def add_booking(
+    room_id: int, date_from: date, date_to: date,
+    user: Users = Depends(get_current_user),                    
+):
+    booking = await BookingDAO.add(user.id, room_id, date_from, date_to)
+    if not booking:
+        raise  RoomCannotBeBooked
